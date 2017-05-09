3c83d2364327ae027942d2ceb2d65933420f558d
Sherya <chattopadhyayshreya@gmail.com>
2017-05-09 16:40:30 +0530

    first

---
+++ src/pages/home/home.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Camera } from '@ionic-native/camera';
+import { NgModule } from '@angular/core';
+
+
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html',
+  providers: [
+ Camera
+ ]
+})
+export class HomePage {
+	
+  public base64Image: string;
+  constructor(public navCtrl: NavController,public camera: Camera) {
+
+  }
+
+    takePicture(){
+    this.camera.getPicture({
+        destinationType: this.camera.DestinationType.DATA_URL,
+        sourceType : this.camera.PictureSourceType.CAMERA,
+        targetWidth: 1000,
+        targetHeight: 1000
+    }).then((imageData) => {
+      // imageData is a base64 encoded string
+        this.base64Image = "data:image/jpeg;base64," + imageData;
+    }, (err) => {
+        console.log(err);
+    });
+  }
+
+}
